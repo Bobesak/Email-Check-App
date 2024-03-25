a90e894fa96362385c5a22c23e0603ec4a1a2abc
Bobes <75141694+Bobesak@users.noreply.github.com>
2024-03-25 08:45:43 +0100

    New String
    Remade the variables into a single one using field

--- MainProgram.cs
+++ MainProgram.cs
@@ -9,14 +9,12 @@ namespace email
         Console.WriteLine("Enter your email address: "); //Vstup emailu
         string mail = Console.ReadLine();
         mail.Trim(); // Odstraneni mezery
-        string koncovka1 = ".com"; //declarovani koncovky emailu
-        string koncovka2 = ".sk";
-        string koncovka3 = ".cz";
+        string[] koncovky = {".sk", ".com", ".cz"}; //Promenne
         int delka = mail.Length;
         bool ContainsAt = mail.Contains("@"); //kontrola zda obsahuje zavinac
-        bool isCom = mail.EndsWith(koncovka1); //Zjisti na kterou koncovku email konci
-        bool isSk = mail.EndsWith(koncovka2);
-        bool isCz = mail.EndsWith(koncovka3);
+        bool isCom = mail.EndsWith(koncovky[1]); //Zjisti na kterou koncovku email konci
+        bool isSk = mail.EndsWith(koncovky[0]);
+        bool isCz = mail.EndsWith(koncovky[2]);
         if (ContainsAt) //Pokud obsahuje zavinac, kod bude pokracovat
         {
             if (isCom)
