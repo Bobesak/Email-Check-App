312957c7acdb7785729ac070e5e1e67ccbb89406
Bobes <75141694+Bobesak@users.noreply.github.com>
2024-03-26 10:42:22 +0100

    Update MainProgram.cs

--- MainProgram.cs
+++ MainProgram.cs
@@ -6,44 +6,44 @@ namespace email
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("Enter your email address: "); //Vstup emailu
-        string mail = Console.ReadLine();
-        mail.Trim(); // Odstraneni mezery
-        string[] koncovky = {".sk", ".com", ".cz"}; //Promenne
-        int delka = mail.Length;
-        bool ContainsAt = mail.Contains("@"); //kontrola zda obsahuje zavinac
-        bool isCom = mail.EndsWith(koncovky[1]); //Zjisti na kterou koncovku email konci
-        bool isSk = mail.EndsWith(koncovky[0]);
-        bool isCz = mail.EndsWith(koncovky[2]);
-        if (ContainsAt) //Pokud obsahuje zavinac, kod bude pokracovat
-        {
-            if (isCom)
-            {
-                Console.WriteLine("International Email");
-                Console.WriteLine(delka + " Symbols Long");
-            }
+            Console.WriteLine("Enter your email address: "); //Vstup emailu
+            string mail = Console.ReadLine();
+            mail.Trim(); // Odstraneni mezery
+            string[] koncovky = {".sk", ".com", ".cz"}; //Promenne
+            int delka = mail.Length;
+            bool ContainsAt = mail.Contains("@"); //kontrola zda obsahuje zavinac
+            bool isCom = mail.EndsWith(koncovky[1]); //Zjisti na kterou koncovku email konci
+            bool isSk = mail.EndsWith(koncovky[0]);
+            bool isCz = mail.EndsWith(koncovky[2]);
+                if (ContainsAt) //Pokud obsahuje zavinac, kod bude pokracovat
+                {
+                    if (isCom)
+                    {
+                        Console.WriteLine("International Email");
+                        Console.WriteLine(delka + " Symbols Long");
+                    }
+                    
+                    else if (isSk)
+                    {
+                        Console.WriteLine("Slovensky Email");
+                        Console.WriteLine("Dĺžka " + delka + " symbolov");
+                    }
+                    
+                    else if (isCz)
+                    {
+                        Console.WriteLine("Cesky Email");
+                        Console.WriteLine(delka + " Symbolu");
+                    }
+                }       
+                else //Pokud neobsahuje zavinac, kod skonci
+                {
+                    Console.WriteLine("Neplatny vstup");      
+                }
             
-            else if (isSk)
-            {
-                Console.WriteLine("Slovensky Email");
-                Console.WriteLine("Dĺžka " + delka + " symbolov");
-            }
-            
-            else if (isCz)
-            {
-                Console.WriteLine("Cesky Email");
-                Console.WriteLine(delka + " Symbolu");
-            }
-        }       
-        else //Pokud neobsahuje zavinac, kod skonci
-        {
-            Console.WriteLine("Neplatny vstup");      
-        }
-        
-        Thread.Sleep(2500); //Zpozdeni aby clovek stihl precist vysledek
+            Thread.Sleep(2500); //Zpozdeni aby clovek stihl precist vysledek
 
 
-        Console.ReadKey();
+            Console.ReadKey();
 
         }
     
